d639ca3667aeb38751ab668da9fe84215775cac3
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-06-22 11:23:44 -0500

    Add CamelCase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
-import Search from "./components/search";
+import Search from "./components/Search";
 import CardList from "./components/CardList";
 
 class App extends Component {
